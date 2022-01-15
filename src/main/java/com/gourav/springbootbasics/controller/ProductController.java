d61a95d692fd0aa3e08a25f1dabac408e3c20f11
Gourav <gourav2824@gmail.com>
2022-01-16 01:36:37 +0530

    Add: add changes for intgeration with products frontend

--- src/main/java/com/gourav/springbootbasics/controller/ProductController.java
+++ src/main/java/com/gourav/springbootbasics/controller/ProductController.java
@@ -15,26 +15,31 @@ public class ProductController {
         this.repository = repository;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/products")
     public List<Product> getProducts() {
         return repository.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/products/{id}")
     public Product getProduct(@PathVariable("id") int id) {
         return repository.findById(id).get();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/products")
     public Product createProduct(@RequestBody Product product){
         return repository.save(product);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/products")
     public Product updateProduct(@RequestBody Product product){
         return repository.save(product);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/products/{id}")
     public void deleteProduct(@PathVariable("id") int id){
         repository.deleteById(id);
